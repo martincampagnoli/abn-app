35da86ec6183190f52825210c671e0b4c204708e
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-10-31 14:41:54 +0100

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+body {
+  background: #00b09b;
+  background: -webkit-linear-gradient(to right, #00b09b, #96c93d);
+  background: linear-gradient(to right, #00b09b, #96c93d);
+  min-height: 100vh;
+}
+
+.text-gray {
+  color: #aaa;
+}
+</style>
