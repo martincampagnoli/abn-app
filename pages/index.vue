185dc3cfec57fcd6028627c9f2b6e4e945b9015b
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-10-31 14:42:08 +0100

    Add default layout

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,3 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
-<style>
-@import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
-body {
-  background: #00b09b;
-  background: -webkit-linear-gradient(to right, #00b09b, #96c93d);
-  background: linear-gradient(to right, #00b09b, #96c93d);
-  min-height: 100vh;
-}
-
-.text-gray {
-  color: #aaa;
-}
-</style>
