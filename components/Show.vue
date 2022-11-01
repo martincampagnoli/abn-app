81c2c65895fcea9ab8c4f701b993347cff17b030
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-11-01 17:42:20 +0100

    Add validations

--- components/Show.vue
+++ components/Show.vue
@@ -5,7 +5,10 @@
         <h5 class="mt-0 font-weight-bold mb-2">{{ show.name }}</h5>
       </NuxtLink>
       <p class="font-italic text-muted mb-0 small">
-        <span v-html="`${show.summary.slice(0, textLimit).trim()}...`"></span>
+        <span
+          v-if="show.summary"
+          v-html="`${show.summary.slice(0, textLimit).trim()}...`"
+        ></span>
       </p>
       <div class="d-flex align-items-center justify-content-between mt-1">
         <h6 class="font-weight-bold my-2">{{ show.language }}</h6>
@@ -18,9 +21,9 @@
       </div>
     </div>
     <img
-      :src="show.image.medium"
+      :src="show.image?.medium"
       width="100"
-      class="ml-lg-5 order-1 order-lg-2"
+      class="ml-lg-5 order-1 order-lg-2 col-sm-3"
     />
   </div>
 </template>
