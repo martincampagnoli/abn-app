b556c6268ff01c805c64e92ebd460534b3e493f3
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-11-02 16:51:43 +0100

    Add some unit tests

--- test/Rating.spec.js
+++ test/Rating.spec.js
@@ -7,4 +7,14 @@ describe('Rating', () => {
     wrapper.setProps({ value: 3 })
     expect(wrapper.vm).toBeTruthy()
   })
+  test('should render stars based on rating > 0', () => {
+    const wrapper = mount(Rating)
+    wrapper.setProps({ value: 3 })
+    expect(wrapper.find('li').exists())
+  })
+  test('should not render stars based on rating 0', () => {
+    const wrapper = mount(Rating)
+    wrapper.setProps({ value: 0 })
+    expect(wrapper.find('li').exists()).toBe(false)
+  })
 })
