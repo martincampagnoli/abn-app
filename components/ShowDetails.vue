3191e984d10b2b784d76365f4c5934dc67a505ad
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-11-02 16:30:29 +0100

    Add validations

--- components/ShowDetails.vue
+++ components/ShowDetails.vue
@@ -9,7 +9,7 @@
                 <div class="text-center p-4">
                   <img
                     id="main-image"
-                    :src="showInfo.image.medium"
+                    :src="showInfo?.image?.medium"
                     width="250"
                   />
                 </div>
@@ -26,23 +26,23 @@
                   </NuxtLink>
                 </div>
                 <div class="mt-4 mb-3">
-                  <h5 class="text-uppercase">{{ showInfo.name }}</h5>
+                  <h5 class="text-uppercase">{{ showInfo?.name }}</h5>
                   <div class="price d-flex flex-row align-items-center">
                     <div class="ml-2">
-                      <span v-for="genre in showInfo.genres" :key="genre.id"
+                      <span v-for="genre in showInfo?.genres" :key="genre.id"
                         >{{ genre }}
                       </span>
                     </div>
                   </div>
                 </div>
-                <p class="about" v-html="showInfo.summary"></p>
+                <p class="about" v-html="showInfo?.summary"></p>
                 <h6 class="font-weight-bold my-2">
-                  Language: {{ showInfo.language }}
+                  Language: {{ showInfo?.language }}
                 </h6>
-                <Rating :value="showInfo.rating.average" />
-                <NuxtLink :to="showInfo.officialSite">
+                <Rating :value="showInfo?.rating?.average" />
+                <NuxtLink :to="showInfo?.officialSite">
                   <h6 class="font-weight-bold my-2">
-                    {{ showInfo.officialSite }}
+                    {{ showInfo?.officialSite }}
                   </h6>
                 </NuxtLink>
               </div>
